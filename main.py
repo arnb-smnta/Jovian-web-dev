e716bd60f4f6ec986b16ef2d9313ab930589f7d4
arnab samanta <110292025+loveumearnab2812000@users.noreply.github.com>
2023-03-19 15:44:03 +0000

    Entered the job items and apply button

--- main.py
+++ main.py
@@ -2,10 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id': 1,
+  "title": "Data Scientist",
+  "location": 'Delhi,india',
+  "salary": 'Rs 1200000',
+}, {
+  "id": 2,
+  "title": "Data-Analyst",
+  "location": "Sanfransisco",
+  "salary": 'Rs 1400000',
+}, {
+  "id": 3,
+  "title": "Frontend-engineer",
+  "location": "Bangalore,India",
+  "salary": 'Rs 1500000'
+}, {
+  "id": 4,
+  "title": "Backend-engineer",
+  "location": "Netherlands",
+  "salary": '$120000'
+}]
+
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html", jobs=JOBS)
 
 
 if __name__ == "__main__":
