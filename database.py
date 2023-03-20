174184dd7d12ea4474519aa2256981057aa0799c
arnab samanta <110292025+loveumearnab2812000@users.noreply.github.com>
2023-03-20 16:30:20 +0000

    Databases changes

--- database.py
+++ database.py
@@ -1,11 +1,19 @@
 import sqlalchemy
-from sqlalchemy import create_engine,text
-engine = create_engine("mysql+pymysql://user:pass@some_mariadb/dbname?charset=utf8mb4")
+from sqlalchemy import create_engine, text
 
+engine = create_engine(
+  "mysql+pymysql://user:pass@some_mariadb/dbname?charset=utf8mb4")
 
 db_connection_string = "mysql+pymysql://mr2zs3g0h9aow47fzcw0:pscale_pw_FajGQTNyYkihzwpENroelI5djE3I4mX17ndtfDMxCe5@ap-south.connect.psdb.cloud/joviancareers?charset=utf8mb4"
-engine=create_engine(db_connection_string,
-                    connect_args={"ssl":{"ssl_ca": "/etc/ssl/cert.pem"}})
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
 with engine.connect() as conn:
-  result=conn.execute(text("select * from jobs"))
-  print(result.all())
+  result = conn.execute(text("select * from jobs"))
+  result_all = result.all()
+  print("type(result.all) : ", type(result.all()))
+  print(type(result_all[0]))
+  first_result_dict = dict(result_all[0])
+  print("type of first result", type(first_result_dict))
+  print(first_result_dict)
