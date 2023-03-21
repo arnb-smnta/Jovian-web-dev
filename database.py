b6d95118703cf6993c79b74cd1235c8df4f6c8e6
arnab samanta <110292025+loveumearnab2812000@users.noreply.github.com>
2023-03-21 09:15:35 +0000

    Dictionary database complete

--- database.py
+++ database.py
@@ -17,8 +17,17 @@ with engine.connect() as conn:
   first_result_dict = result_all[0][0]
   print("type of first result", type(first_result_dict))
   print(first_result_dict)
-  result_dict={}
+  result_dict = {}
+  result_ld = []
+  i = 0
   for result in result_all:
-    result_dict['id']=result[0]
-
-  print(result_dict)
+    result_dict['id'] = result[0]
+    result_dict['title'] = result[1]
+    result_dict['location'] = result[2]
+    result_dict["salary"] = result[3]
+    result_dict['currency'] = result[4]
+    result_dict['responsibilities'] = result[5]
+    result_dict['requirements'] = result[6]
+    print(result_dict)
+    result_ld.append(result_dict.copy())
+  print(result_ld)
