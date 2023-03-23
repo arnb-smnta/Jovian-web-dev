14cbd8c2f4ea74caa412f737398f33e84956f449
arnab samanta <110292025+loveumearnab2812000@users.noreply.github.com>
2023-03-23 14:57:06 +0000

    Crated secret

--- database.py
+++ database.py
@@ -1,12 +1,8 @@
-import sqlalchemy
+import sqlalchemy, os
 from sqlalchemy import create_engine, text
 
-engine = create_engine(
-  "mysql+pymysql://user:pass@some_mariadb/dbname?charset=utf8mb4")
-
-db_connection_string = "mysql+pymysql://mr2zs3g0h9aow47fzcw0:pscale_pw_FajGQTNyYkihzwpENroelI5djE3I4mX17ndtfDMxCe5@ap-south.connect.psdb.cloud/joviancareers?charset=utf8mb4"
+db_connection_string = os.environ['db_connection_string']
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
                          "ssl_ca": "/etc/ssl/cert.pem"
                        }})
-
